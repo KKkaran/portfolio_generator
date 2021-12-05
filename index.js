2b54b0af815fa50d11578e88e1bd7a0316809212
Karan Sodhi <karanpreetsodhi1997@gmail.com>
2021-12-05 17:12:45 -0500

    var vs let

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
-console.log(process.argv.slice(2,process.argv.length))
+const profileDataArgs = process.argv.slice(2,process.argv.length)
+
+const printProfileData = (profileDataArr) => profileDataArr.forEach(e => console.log(e))
+  
+printProfileData(profileDataArgs);
+
+// const addThem = (a,b) => a + b //an implicit return, not using return keyword
+// console.log(addThem(3,4))
