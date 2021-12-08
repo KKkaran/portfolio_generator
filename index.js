b2f10b057113f07a1193a9f9ef2ee15e29ab54d3
Karan Sodhi <karanpreetsodhi1997@gmail.com>
2021-12-07 22:38:02 -0500

    created gitignore file to not upload node_modules to github

--- index.js
+++ index.js
@@ -1,19 +1,15 @@
 const maths = require("./maths");
 const fs = require("fs");
 const inquirer = require("inquirer")
+const moment = require("moment")
 const createHtml = require("./src/page-template")
 //const profileDataArgs = process.argv.slice(2,process.argv.length)
 
-inquirer.prompt("What is your name?").then(ans=>{
-    console.log("so your name is : " + ans)
-})
-
 //console.log(maths)    
-
 //writes into the file
 fs.writeFile("index.html", createHtml.generatehtml(process.argv[2],process.argv[3]), error =>{
    
-    error ? console.log(error) : console.log("portfolio created now!!!")
+    error ? console.log(error) : console.log("portfolio created now!!!\n" + moment().format("MM do YYYY"))
 })
 
 //reads the file
